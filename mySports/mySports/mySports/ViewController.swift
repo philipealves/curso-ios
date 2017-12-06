d38279df8510ba4d6182faf634a1d9a8ea89bbb6
Philipe Alves de Oliveira e Silva <philipealvess@gmail.com>
2017-12-06 16:03:17 -0200

    Initial commit

---
+++ mySports/mySports/mySports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mySports
+//
+//  Created by ALOC SP05816 on 06/12/2017.
+//  Copyright © 2017 Philipe Alves de Oliveira e SIlva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ mySports/mySportsData/mySportsData/mySportsData.h
@@ -0,0 +1,19 @@
+//
+//  mySportsData.h
+//  mySportsData
+//
+//  Created by ALOC SP05816 on 06/12/2017.
+//  Copyright © 2017 Philipe Alves de Oliveira e SIlva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mySportsData.
+FOUNDATION_EXPORT double mySportsDataVersionNumber;
+
+//! Project version string for mySportsData.
+FOUNDATION_EXPORT const unsigned char mySportsDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mySportsData/PublicHeader.h>
+
+
